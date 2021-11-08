16a9e8806c30e07b0439b3f08dd2632c105c8fa3
Tomas Krajcoviech <tkrajcoviech@sjsk.local>
2021-11-08 15:28:56 +0100

    fixes

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from dateutil.relativedelta import relativedelta
 from pprint import pprint as pp
 import sys
 
+MAX_DATA = 300
+
 a = cbpro.PublicClient()
 
 def get_candles(gran: str, start, end):
@@ -14,29 +16,29 @@ def get_candles(gran: str, start, end):
     if gran not in acceptedGrans:
         raise ValueError(f'selected {gran!r} is not in {acceptedGrans=}')
     grans = acceptedGrans.get(gran)
-    max_candles = grans // 60 * 300
+    delta_time_candles = grans // 60 * MAX_DATA
     until_time = end
     from_time = start
     diff = end - start
     t_seconds = diff.total_seconds()
     req_candles = t_seconds // grans
-    # print(f'{diff=}\n{diff.total_seconds()=}\n{req_candles=}')
+    print(f'{diff=}\n{diff.total_seconds()=}\n{req_candles=}\n{delta_time_candles=}')
     last_stamp = end
-    if req_candles > max_candles:
+    if req_candles > MAX_DATA:
         print(f'size is too big... adjusting from time...')
-        from_time = end - relativedelta(minutes=max_candles)
+        from_time = end - relativedelta(minutes=delta_time_candles)
         last_stamp = from_time
-        # from_time = from_time.replace(tzinfo=tz.tzlocal())
-        print(f'{from_time=}\n{start=}')
+        # print(f'{from_time=}\n{start=}')
 
     ll = []
-    print(f'{last_stamp=!r}\n{start=!r}')
+    # print(f'{last_stamp=!r}\n{start=!r}')
     while last_stamp > start:
-        # print(f'target date {end!r}\nstarting {from_time=} {until_time=}')
-        prices = a.get_product_historic_rates('ADA-EUR',from_time,until_time,grans)
+        prices = a.get_product_historic_rates('BTC-EUR',from_time,until_time,grans)
         # print(prices)
+        if prices == []:
+            break
         ll.append(prices)
-        # print(prices[1:10])
+        print(prices[1:3])
         print(len(prices))
         print(f'{prices[0]=}\t\t{datetime.datetime.fromtimestamp(prices[0][0])}\
             \n{prices[-1]=}\t\t{datetime.datetime.fromtimestamp(prices[-1][0])}')
@@ -45,19 +47,18 @@ def get_candles(gran: str, start, end):
         print(f'{last_stamp=}')
         # update times
         until_time = last_stamp
-        # until_time = until_time.replace(tzinfo=tz.tzlocal())
         remain_diff = (until_time - start)
         remain_diff = remain_diff.total_seconds()
-        if remain_diff > max_candles:
-            from_time = until_time - relativedelta(minutes=max_candles)
-            # from_time = from_time.replace(tzinfo=tz.tzlocal())
+        print(f'Remaining time is {remain_diff}')
+        if remain_diff > MAX_DATA:
+            from_time = until_time - relativedelta(minutes=delta_time_candles)
         else:
             from_time = end
     ll = [_x for _y in ll for _x in _y]
     print(len(ll))
-    print(ll)
+    # print(ll)
 
 end = datetime.datetime.now(tz = tz.tzlocal())
-start = datetime.datetime.fromisoformat('2021-11-08 13:15:00')
+start = datetime.datetime.fromisoformat('2018-06-01 00:45:00')
 start = start.replace(tzinfo=tz.tzlocal())
-get_candles('minute',start,end)
+get_candles('day',start,end)
